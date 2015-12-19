70a5c9ab46eaf747d75ada4aeb7bc3d417fc53d8
Trent Oswald <trentoswald@therebelrobot.com>
2015-12-18 16:21:37 -0800

    added select2 to support tagging

--- js/main.js
+++ js/main.js
@@ -2,16 +2,28 @@ var request = window.superagent
 
 $(document).ready(mainApp)
 function mainApp () {
-  var ideas = m.prop({})
+  var ideas = m.prop([])
+  var thisIdea = m.prop({
+    title: 'Just Build Something',
+    description: ['Sometimes you just need an idea. ',
+      m('em', 'Any'),
+      ' idea.',
+      m('br'),
+      "We've got you covered."
+    ],
+    additional_details: null
+  })
+  var anotherIdea = m.prop(false)
   var JBS = {
     // controller
     controller: function () {
-      request.get('/ideas.json').end(function(err, data){
-        if(err){
+      request.get('/ideas.json').end(function (err, data) {
+        if (err) {
           throw new Error(err)
         }
-        ideas(data)
+        ideas(data.body)
         console.log(ideas())
+        m.redraw(true)
       })
       return {}
     },
@@ -30,10 +42,24 @@ function mainApp () {
                 ' Something        '
               ])
             ]),
-            m('.collapse.navbar-collapse.navbar-right.navbar-main-collapse', [
-              m('ul.nav.navbar-nav', [
-                m('li', [
-                  m("input.form-control[placeholder='Tag Search'][type='text']")
+            m('.collapse.navbar-collapse.navbar-right.col-md-6', [
+              m('ul.nav.navbar-nav.col-md-3', [
+                m('li.col-md-12', [
+                  m('select.form-control', {
+                    config: function (el, isinit, context) {
+                      if (isinit) return
+                      console.log(el)
+                      $(el).select2({
+                        tags: true,
+                        tokenSeparators: [','],
+                        placeholder: 'Search by tag',
+                        allowClear:true
+                      })
+                    },
+                    multiple:"multiple"
+                  }, _.map(ideas(), function (idea) {
+                    return m('option', idea.title)
+                  }))
                 ])
               ])
             ])
@@ -44,15 +70,22 @@ function mainApp () {
             m('.container', [
               m('.row', [
                 m('.col-md-8.col-md-offset-2', [
-                  m('h1.brand-heading', 'Just Build Something'),
-                  m('p.intro-text', ['Sometimes you just need an idea. ',
-                    m('em', 'Any'),
-                    ' idea.',
-                    m('br'),
-                    'We\'ve got you covered.'
-                  ]),
-                  m("a.btn.btn-circle.page-scroll[href='#']", [
+                  m('h1.brand-heading', thisIdea().title),
+                  m('p.intro-text', thisIdea().description),
+                  m("a.btn.btn-circle.page-scroll[href='#']", {
+                    config: function (el, isInit, context) {
+                      if (isInit) return
+                      console.log('initing')
+                      el.onclick = function (event) {
+                        console.log('clicking')
+                        getIdea()
+                      }
+                    }
+                  }, [
                     'Give me something ',
+                    m('span', {
+                      className: anotherIdea()? '': 'hidden'
+                    }, 'else '),
                     m('i.fa.fa-angle-double-up.animated')
                   ])
                 ])
@@ -65,4 +98,17 @@ function mainApp () {
   }
   // initialize
   m.mount(document.getElementById('justbuildsomething'), JBS)
+  var getIdea = function (tag) {
+    var tag = tag
+    anotherIdea(true)
+    if (!tag) {
+      var randomIndex = getRandom(0, ideas().length - 1)
+      thisIdea(ideas()[randomIndex])
+      m.redraw(true)
+    }
+  }
+}
+
+function getRandom (min, max) {
+  return Math.floor(Math.random() * (max - min) + min)
 }
